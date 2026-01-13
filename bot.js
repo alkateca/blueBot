8370346b45e5216468b44bb9569760b17981ab47
alkateca <chesini.mendes@gmail.com>
2026-01-13 16:27:42 -0300

    corrigido erro no login

--- bot.js
+++ bot.js
@@ -3,18 +3,25 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-// Create a Bluesky Agent 
+const { BLUESKY_USERNAME, BLUESKY_PASSWORD } = process.env;
+
 const agent = new BskyAgent({
     service: 'https://bsky.social',
   })
 
 
 async function main() {
-    await agent.login({ BLUESKY_USERNAME, BLUESKY_PASSWORD})
-    await agent.post({
-        text: "🙂"
-    });
-    console.log("Just posted!");
+    try {
+        
+        await agent.login({ identifier: BLUESKY_USERNAME, password: BLUESKY_PASSWORD})
+        await agent.post({
+            text: "🙂"
+        });
+       console.log("Just posted!");
+    } catch (err) {
+        console.error("Error: ", err);
+        process.exit(1);
+    }
 }
 
 main();
